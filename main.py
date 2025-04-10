39d9d4b8ba0457c68b3822b4bcf7c34490d21fde
sebsanchez-18 <sebastianes1312@gmail.com>
2025-04-10 11:12:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 from pulsesensor import Pulsesensor
 import time
 
-def main():
+if __name__ == "__main__":
     sensor = Pulsesensor(channel=0, address=0x48)
-    sensor.startAsyncBPM()
-
     try:
+        sensor.startAsyncBPM()
         while True:
-            # Print the current BPM value every second.
-            print("Current BPM from main loop:", sensor.BPM, flush=True)
+            print(f"Current BPM: {sensor.BPM}")
             time.sleep(1)
     except KeyboardInterrupt:
+        print("Exiting...")
+    finally:
         sensor.stopAsyncBPM()
-        print("Stopped BPM measurement.", flush=True)
-
-if __name__ == '__main__':
-    main()
