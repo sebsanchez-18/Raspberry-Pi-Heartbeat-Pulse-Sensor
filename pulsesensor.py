37205e63355bef4e13e441cfe7f72e1fa01b8856
sebsanchez-18 <sebastianes1312@gmail.com>
2025-04-15 11:44:47 -0400

    Update pulsesensor.py

--- pulsesensor.py
+++ pulsesensor.py
@@ -1,102 +1,101 @@
-# extended from https://github.com/WorldFamousElectronics/PulseSensor_Amped_Arduino
-
+# PulseSensor using ADS1015 (I2C ADC)
 import time
 import threading
-from MCP3008 import MCP3008
+import board
+import busio
+from adafruit_ads1x15.ads1015 import ADS1015
+from adafruit_ads1x15.analog_in import AnalogIn
 
 class Pulsesensor:
-    def __init__(self, channel = 0, bus = 0, device = 0):
+    def __init__(self, channel=0):
         self.channel = channel
         self.BPM = 0
-        self.adc = MCP3008(bus, device)
+
+        # Setup I2C and ADS1015
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.ads = ADS1015(i2c)
+        self.ads.gain = 1
+
+        # Channel selection (0–3)
+        self.chan = AnalogIn(self.ads, getattr(ADS1015, f'P{channel}'))
+
+        self._stop_event = threading.Event()
 
     def getBPMLoop(self):
-        # init variables
-        rate = [0] * 10         # array to hold last 10 IBI values
-        sampleCounter = 0       # used to determine pulse timing
-        lastBeatTime = 0        # used to find IBI
-        P = 512                 # used to find peak in pulse wave, seeded
-        T = 512                 # used to find trough in pulse wave, seeded
-        thresh = 525            # used to find instant moment of heart beat, seeded
-        amp = 100               # used to hold amplitude of pulse waveform, seeded
-        firstBeat = True        # used to seed rate array so we startup with reasonable BPM
-        secondBeat = False      # used to seed rate array so we startup with reasonable BPM
+        rate = [0] * 10
+        sampleCounter = 0
+        lastBeatTime = 0
+        P = 512
+        T = 512
+        thresh = 525
+        amp = 100
+        firstBeat = True
+        secondBeat = False
 
-        IBI = 600               # int that holds the time interval between beats! Must be seeded!
-        Pulse = False           # "True" when User's live heartbeat is detected. "False" when not a "live beat". 
-        lastTime = int(time.time()*1000)
-        
-        while not self.thread.stopped:
-            Signal = self.adc.read(self.channel)
-            currentTime = int(time.time()*1000)
-            
+        IBI = 600
+        Pulse = False
+        lastTime = int(time.time() * 1000)
+
+        while not self._stop_event.is_set():
+            Signal = self.chan.value // 256  # scale 16-bit to 10-bit range roughly
+
+            currentTime = int(time.time() * 1000)
             sampleCounter += currentTime - lastTime
             lastTime = currentTime
-            
             N = sampleCounter - lastBeatTime
 
-            # find the peak and trough of the pulse wave
-            if Signal < thresh and N > (IBI/5.0)*3:     # avoid dichrotic noise by waiting 3/5 of last IBI
-                if Signal < T:                          # T is the trough
-                    T = Signal                          # keep track of lowest point in pulse wave 
+            if Signal < thresh and N > (IBI / 5.0) * 3:
+                if Signal < T:
+                    T = Signal
 
             if Signal > thresh and Signal > P:
                 P = Signal
 
-            # signal surges up in value every time there is a pulse
-            if N > 250:                                 # avoid high frequency noise
-                if Signal > thresh and Pulse == False and N > (IBI/5.0)*3:       
-                    Pulse = True                        # set the Pulse flag when we think there is a pulse
-                    IBI = sampleCounter - lastBeatTime  # measure time between beats in mS
-                    lastBeatTime = sampleCounter        # keep track of time for next pulse
+            if N > 250:
+                if Signal > thresh and not Pulse and N > (IBI / 5.0) * 3:
+                    Pulse = True
+                    IBI = sampleCounter - lastBeatTime
+                    lastBeatTime = sampleCounter
 
-                    if secondBeat:                      # if this is the second beat, if secondBeat == TRUE
-                        secondBeat = False;             # clear secondBeat flag
-                        for i in range(len(rate)):      # seed the running total to get a realisitic BPM at startup
-                          rate[i] = IBI
+                    if secondBeat:
+                        secondBeat = False
+                        rate = [IBI] * len(rate)
 
-                    if firstBeat:                       # if it's the first time we found a beat, if firstBeat == TRUE
-                        firstBeat = False;              # clear firstBeat flag
-                        secondBeat = True;              # set the second beat flag
+                    if firstBeat:
+                        firstBeat = False
+                        secondBeat = True
                         continue
 
-                    # keep a running total of the last 10 IBI values  
-                    rate[:-1] = rate[1:]                # shift data in the rate array
-                    rate[-1] = IBI                      # add the latest IBI to the rate array
-                    runningTotal = sum(rate)            # add upp oldest IBI values
+                    rate[:-1] = rate[1:]
+                    rate[-1] = IBI
+                    runningTotal = sum(rate)
+                    runningTotal /= len(rate)
+                    self.BPM = 60000 / runningTotal
 
-                    runningTotal /= len(rate)           # average the IBI values 
-                    self.BPM = 60000/runningTotal       # how many beats can fit into a minute? that's BPM!
-
-            if Signal < thresh and Pulse == True:       # when the values are going down, the beat is over
-                Pulse = False                           # reset the Pulse flag so we can do it again
-                amp = P - T                             # get amplitude of the pulse wave
-                thresh = amp/2 + T                      # set thresh at 50% of the amplitude
-                P = thresh                              # reset these for next time
+            if Signal < thresh and Pulse:
+                Pulse = False
+                amp = P - T
+                thresh = amp / 2 + T
+                P = thresh
                 T = thresh
 
-            if N > 2500:                                # if 2.5 seconds go by without a beat
-                thresh = 512                            # set thresh default
-                P = 512                                 # set P default
-                T = 512                                 # set T default
-                lastBeatTime = sampleCounter            # bring the lastBeatTime up to date        
-                firstBeat = True                        # set these to avoid noise
-                secondBeat = False                      # when we get the heartbeat back
+            if N > 2500:
+                thresh = 512
+                P = 512
+                T = 512
+                lastBeatTime = sampleCounter
+                firstBeat = True
+                secondBeat = False
                 self.BPM = 0
 
             time.sleep(0.005)
-            
-        
-    # Start getBPMLoop routine which saves the BPM in its variable
+
     def startAsyncBPM(self):
+        self._stop_event.clear()
         self.thread = threading.Thread(target=self.getBPMLoop)
-        self.thread.stopped = False
         self.thread.start()
-        return
-        
-    # Stop the routine
+
     def stopAsyncBPM(self):
-        self.thread.stopped = True
+        self._stop_event.set()
+        self.thread.join()
         self.BPM = 0
-        return
-    
